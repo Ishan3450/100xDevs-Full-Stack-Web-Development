8069e329baefa1ca4bd195716bdd81c43f648f28
Ishan3450 <jaganiishan@gmail.com>
2024-05-28 13:46:51 +0530

    Added Week 2 Node.js Assignment

---
+++ Week 2/0-Assignments/02-nodejs/fileServer.js
@@ -0,0 +1,55 @@
+/**
+  You need to create an express HTTP server in Node.js which will handle the logic of a file server.
+  - Use built in Node.js `fs` module
+  The expected API endpoints are defined below,
+  1. GET /files - Returns a list of files present in `./files/` directory
+    Response: 200 OK with an array of file names in JSON format.
+    Example: GET http://localhost:3000/files
+  2. GET /file/:filename - Returns content of given file by name
+     Description: Use the filename from the request path parameter to read the file from `./files/` directory
+     Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. Should return `File not found` as text if file is not found
+     Example: GET http://localhost:3000/file/example.txt
+    - For any other route not defined in the server return 404
+    Testing the server - run `npm run test-fileServer` command in terminal
+ */
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+
+const dirPath = "D:/100xDevs FullStack/Week 2/0-Assignments/02-nodejs/files";
+
+app.get("/files", function (req, res) {
+  fs.readdir(
+    dirPath, // full path of the dir is necessary
+    "utf-8",
+    function (err, files) {
+      if (err) {
+        res.status(404).send("Not found");
+      } else {
+        res.status(200).json(files);
+      }
+    }
+  );
+});
+
+app.get("/file/:filename", function (req, res) {
+  const fileName = req.params.filename;
+  const filePath = path.join(dirPath, `/${fileName}`);
+
+  fs.readFile(filePath, "utf-8", function (err, data) {
+    if (err) {
+      return res.status(404).send("File not found");
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.get("*", function (req, res) {
+  res.status(404).send("Route not found");
+});
+
+// app.listen(3001);
+
+module.exports = app;
---
+++ Week 2/1-test-server/README.md
@@ -0,0 +1,11 @@
+# Ways to create a new node project and creating new HTTP server with express
+
+```bash
+npm init -y
+```
+package.json will be created after executing above command in the terminal
+
+To install express
+```bash
+npm install express
+```
